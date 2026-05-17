65dfa1b445a87e11fd3af553c6088089defc4d3e
ANNGELO3000 <angelosrielherreratola@gmail.com>
2026-05-16 21:21:57 -0500

    conectar base de datos Railway

--- conexion.php
+++ conexion.php
@@ -1,10 +1,5 @@
 <?php
-$host = "127.0.0.1";
-$usuario = "root";
-$contrasena = "root";
-$base_datos = "elotro";
-
-$conn = new mysqli($host, $usuario, $contrasena, $base_datos, 3307);
+$conn = new mysqli('mysql.railway.internal', 'root', 'bGXMLUEcTYTxWGJySgNVJVirwcDmaTOP', 'railway', 3306);
 
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
